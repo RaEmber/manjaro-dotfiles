eaf6165e2a2967e152e3ad33031b6abfb45a5c9f
RaEmber <raphael.emberger@hotmail.ch>
2017-07-11 00:05:18 +0200

    Integrated all config files I found in $HOME

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 stow config
-stow shell
+mkdir -p ~/scripts
+stow scripts -t ~/scripts
 stow colors
+mkdir -p ~/.oh-my-zsh
+mkdir -p ~/.oh-my-zsh/themes
 stow zsh_theme -t ~/.oh-my-zsh/themes
+stow bspwm -t ~/.config/bspwm
+stow sxhkd -t ~/.config/sxhkd
 stow texmaker -t ~/Dokumente
 echo "Done symlinking"
