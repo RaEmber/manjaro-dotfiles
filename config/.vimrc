6879396d2b4d911a8e438bdf215d318b10887825
RaEmber <raphael.emberger@hotmail.ch>
2017-07-10 23:05:18 +0200

    Migrated some of my old Arch Linux configs

---
+++ config/.vimrc
@@ -0,0 +1,6 @@
+set nu
+syntax enable
+set ts=4
+colorscheme vimcol
+
+set mouse=a
--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
-
+stow config
+stow shell
+stow colors
+stow zsh_theme -t ~/.oh-my-zsh/themes
+stow texmaker -t ~/Dokumente
 echo "Done symlinking"
