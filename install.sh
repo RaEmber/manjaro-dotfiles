c2abce5e566ddb91dbb7ed1a29eb9fd6a7730804
raember <raphael.emberger@hotmail.ch>
2017-07-30 13:16:21 +0200

    Fixed file mod and changed install script

--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@ stow bin
 stow bspwm
 ~/.config/bspwm/bspwmrc
 stow color
-stow compton
 stow dmenu
 stow feh
 stow GTKmenu
 stow gtk
-stow limepanel
 stow ls_colors
 stow nano
 stow polybar
+stow ranger
 stow shell
 sudo stow slim -t /etc
 stow sxhkd
 stow texmaker
-stow tmux
 stow vim
 stow X
 xrdb -load ~/.Xresources
