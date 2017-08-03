a140a55d3bf0733a0d4cfe89c942bfaecb8d17e2
raember <raphael.emberger@hotmail.ch>
2017-08-04 00:14:40 +0200

    Changed install script for firefox profile selection. Moveed user.js and created dummy userChrome.css

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
+source <(grep Path\= ~/.mozilla/firefox/profiles.ini)
+profilepath=~/.mozilla/firefox/$Path/
+
 echo "Symlinking packages..."
 stow bin
 stow bspwm
-~/.config/bspwm/bspwmrc
 stow color
 stow dmenu
 stow feh
-stow firefox -t ~/.mozilla/firefox/*.*/
+stow firefox -t $profilepath
 stow GTKmenu
 stow gtk
 stow ls_colors
@@ -20,4 +24,5 @@ stow vim
 stow X
 xrdb -load ~/.Xresources
 stow yaourt
+~/.config/bspwm/bspwmrc
 echo "Done symlinking"
